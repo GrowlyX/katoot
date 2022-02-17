39bc018e8014430c7fbeb6161bf453bebec7f6c0
GrowlyX <growlyxdev@gmail.com>
2022-02-16 19:55:16 -0500

    Map quiz & questions

---
+++ src/main/kotlin/gg/growly/katoot/question/Question.kt
@@ -0,0 +1,19 @@
+package gg.growly.katoot.question
+
+import com.google.gson.annotations.SerializedName
+import gg.growly.katoot.quiz.type.QuizType
+
+/**
+ * @author GrowlyX
+ * @since 2/16/2022
+ */
+data class Question(
+    val type: QuizType,
+    @SerializedName("question")
+    val value: String,
+    // points
+    val points: Boolean,
+    val pointsMultiplier: Int,
+    // choices
+    val choices: List<QuestionChoice>
+)
--- src/main/kotlin/gg/growly/katoot/quiz/Quiz.kt
+++ src/main/kotlin/gg/growly/katoot/quiz/Quiz.kt
@@ -1,6 +1,8 @@
 package gg.growly.katoot.quiz
 
 import com.google.gson.annotations.SerializedName
+import gg.growly.katoot.question.Question
+import gg.growly.katoot.quiz.type.QuizType
 import java.util.UUID
 
 /**
@@ -9,5 +11,17 @@ import java.util.UUID
  */
 class Quiz(
     @SerializedName("uuid")
-    val uniqueId: UUID
+    val uniqueId: UUID,
+    @SerializedName("title")
+    val displayName: String,
+    val description: String?,
+    val quizType: QuizType,
+    // timestamps
+    val created: Long,
+    val modified: Long,
+    // creator information
+    val creator: UUID,
+    val creatorUsername: String,
+    // questions
+    val questions: List<Question>,
 )
